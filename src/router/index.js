bd0a356e8962d13bf8f3ba4470a0d6972095a335
zhang wentao <17810234576@163.com>
2018-06-01 07:01:48 +0000

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -48,7 +48,7 @@ const router = new Router({
         meta: {
             requireAuth: true,  // 添加该字段，表示进入这个路由是需要登录的
         },
-        redirect:'extract',
+        redirect:'extract', 
         children: [{
             path: '/extract',
             name: '提现审核',
